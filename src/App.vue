17c6a27cff22b0d74633510df9a00d101cfefb00
Khan_Viacheslaw <viacheslaw.khan@travelata.ru>
2021-07-26 18:47:37 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="first" @click="inc">+</button>
+    <button class="second" @click="dec">-</button>
+    <button class="hide" @click="toggle">toggle</button>
+    <template v-if="show">
+      {{ counter }}
+    </template>
+    <br>
+    <div class="app-4">
+      <ol>
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      counter: 0,
+      show: true,
+      todos: [
+        { text: 'cat', id: 1 },
+        { text: 'cat1', id: 2 },
+        { text: 'cat2', id: 3 },
+      ],
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+    inc() {
+      this.counter += 1;
+    },
+    dec() {
+      this.counter -= 1;
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
